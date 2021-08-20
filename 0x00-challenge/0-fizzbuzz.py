ae3b2bbb3cf14af0a8989e16e5fe7220253bd0f9
RimJoudi <77971241+RimJoudi@users.noreply.github.com>
2021-08-20 11:45:29 +0100

    fix

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -19,7 +19,7 @@ def fizzbuzz(n):
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
-        if (i % 3) == 0:
+        elif (i % 3) == 0:
             tmp_result.append("Fizz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
