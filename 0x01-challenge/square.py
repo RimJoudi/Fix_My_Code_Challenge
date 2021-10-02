c519d3483b44f1095af2a83897aa51baa87807d0
RimJoudi <joudirim93@gmail.com>
2021-10-02 23:40:30 +0100

    fix square task

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-
+"""
+square module
+"""
 
 class square():
+    """
+    square class
+    """
 
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """
+        Initializer method
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,14 +23,19 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """
+        perimeter of the square fn
+        """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """
+        string representation returns a human-readable format
+        """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
